cbd0b5d526fdaf4a125d2f5f94aec8f7d511f7cf
rajkonkret <rajkonkret660@gmail.com>
2025-08-25 12:28:14 +0200

    [1 6] update pierwszy.py add modulo

--- pierwszy.py
+++ pierwszy.py
@@ -110,3 +110,50 @@ print(bool(""))  # False
 
 # None - nie wiem, stan nieokreslony, odpowidnik null
 print(bool(None))  # False
+
+temp = 36.6
+print(type(temp))  # <class 'float'>
+print(temp)  # 36.6
+print(sys.float_info)
+# sys.float_info(max=1.7976931348623157e+308,
+#                max_exp=1024, max_10_exp=308,
+#                min=2.2250738585072014e-308, min_exp=-1021,
+#                min_10_exp=-307, dig=15, mant_dig=53,
+#                epsilon=2.220446049250313e-16, radix=2, rounds=1)
+
+# bład zaokrąglenia
+print(0.1 + 0.9)  # 1.0
+print(0.1 + 0.2)  # 0.30000000000000004
+#  For example, in a floating-point arithmetic with five base-ten digits,
+#  the sum 12.345 + 1.0001 = 13.3451 might be rounded to 13.345.
+
+# decimal - ominięcie problemu zaokrąglenia, powolne, zajmują dużo miejsca
+
+# f - string format
+print(f"Nazywam się {name}. Dzien dobry")  # Nazywam się 90. Dzien dobry
+print("Nazywam się {}.".format(name))  # Nazywam się 90
+
+liczba = 3.900001
+print(f"Wersja pythona: {liczba}")  # Wersja pythona: 3.900001
+print(f"Wersja pythona: {liczba:.2f}")  # Wersja pythona: 3.90
+
+print(f"""tekst
+    wielolinijkowy""")
+# "tekst
+#     wielolinijkowy"
+
+"""Komentarz 
+wielolinijkowy.
+Komentarz dokumentacyjny"""
+print(print.__doc__)
+
+starszy = "Mam na imię %s"
+print(starszy % name)  # Mam na imię 90
+
+print("Wynik:", liczba)  # Wynik: 3.900001,  sep=' ', end='\n'
+print("Wynik:", liczba, sep="....")  # Wynik:....3.900001 - zmieniony separator
+
+print(100 / 3) # 33.333333333333336
+print(100 // 3) # 33 - częśc całkowita z dzielenia
+print(100 % 3) # modulo, reszta  z dzielenia, r=1
+print(10 % 3) # 1 reszty 3 * 3 + 1 = 10
